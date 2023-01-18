e895629fa3d58dda586f98fc1f9276ef6ad5ae5d
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-01-18 16:37:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@ cd $HOME
 
 mkdir -p .local/bin
 wget -o .local/bin/dns https://raw.githubusercontent.com/codeccoop/hosts-cli/main/hosts-cli.sh
+chmod +x .local/bin/dns
 
 mkdir -p .local/share/bash-completion/completions
 wget -o .local/share/bash-completion/completions/hosts-cli https://raw.githubusercontent.com/codeccoop/hosts-cli/main/bash-completion.sh
 
-config_file="$(test -f ".bashrc" && echo ".bashrc" || ".profile")"
+config_file="$(test -f ".bashrc" && echo ".bashrc" || echo ".profile")"
 echo '# hosts-cli completion script' >> $config_file
 echo 'source ~/.local/share/bash-completion/completions/hosts-cli' >> $config_file
 
